552363863a9fa6614a60917acc973d6064d074f5
nikola <nikola@30-9-3.wireless.csail.mit.edu>
2022-10-27 12:36:33 -0400

    Add support for creating Virt to Phys page mappings

--- vlajko_os/src/main.rs
+++ vlajko_os/src/main.rs
@@ -12,31 +12,17 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+  use vlajko_os::memory::BootInfoFrameAllocator;
   use vlajko_os::memory;
   use x86_64::{structures::paging::Translate, VirtAddr};
+  use x86_64::{structures::paging::Page};
 
   println!("Hello World!");
   vlajko_os::init();
 
   let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-  let mapper = unsafe { memory::init(phys_mem_offset) };
-
-  let addresses = [
-      // vga buffer page
-      0xb8000,
-      // part of code segment
-      0x201008,
-      // part of stack segment
-      0x0100_0020_1a10,
-      // virtual address mapped to physical address 0
-      boot_info.physical_memory_offset,
-  ];
-
-  for &address in &addresses {
-      let virt =  VirtAddr::new(address);
-      let phys = mapper.translate_addr(virt);
-      println!("{:?} -> {:?}", virt, phys);
-  }
+  let mut mapper = unsafe { memory::init(phys_mem_offset) };
+  let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
   #[cfg(test)]
   test_main();
