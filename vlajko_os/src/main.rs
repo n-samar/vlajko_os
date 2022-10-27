dfa0c91a3ef70bd4bfa657f3f8834aa937d745e1
nikola <nikola@30-9-3.wireless.csail.mit.edu>
2022-10-27 11:21:55 -0400

    Implement virtual address translation

--- vlajko_os/src/main.rs
+++ vlajko_os/src/main.rs
@@ -12,19 +12,30 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-  use vlajko_os::memory::active_level_4_table;
-  use x86_64::VirtAddr;
+  use vlajko_os::memory;
+  use x86_64::{structures::paging::Translate, VirtAddr};
 
   println!("Hello World!");
   vlajko_os::init();
 
   let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-  let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+  let mapper = unsafe { memory::init(phys_mem_offset) };
 
-  for (i, entry) in l4_table.iter().enumerate() {
-      if !entry.is_unused() {
-          println!("L4 Entry {}: {:?}", i, entry);
-      }
+  let addresses = [
+      // vga buffer page
+      0xb8000,
+      // part of code segment
+      0x201008,
+      // part of stack segment
+      0x0100_0020_1a10,
+      // virtual address mapped to physical address 0
+      boot_info.physical_memory_offset,
+  ];
+
+  for &address in &addresses {
+      let virt =  VirtAddr::new(address);
+      let phys = mapper.translate_addr(virt);
+      println!("{:?} -> {:?}", virt, phys);
   }
 
   #[cfg(test)]
